4d42b793d2b57bfbf92534e5f47444aba5c7633f
Crystyanno <crystyanno.alves@gmail.com>
2025-08-21 22:24:23 -0300

    add the appbar

--- lib/appbar.dart
+++ lib/appbar.dart
@@ -3,6 +3,15 @@ import 'package:flutter/material.dart';
 class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return AppBar(title: const Text('Minha Landing Page'), centerTitle: true);
+    return Container(
+      padding: const EdgeInsets.all(26.0),
+      child: Column(
+        children: [
+          const Text('Minha Landing Page', style: TextStyle(fontSize: 24)),
+          const SizedBox(height: 8),
+          const Text('Bem-vindo à nossa landing page!'),
+        ],
+      ),
+    );
   }
 }
