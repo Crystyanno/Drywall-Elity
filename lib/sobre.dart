2e47550108c1cf5182adbeccbb95c1b1ba1d24bc
Crystyanno <crystyanno.alves@gmail.com>
2025-08-31 16:36:33 -0300

    updating the file about

--- lib/sobre.dart
+++ lib/sobre.dart
@@ -1,1 +1,12 @@
 
+import 'package:flutter/material.dart';
+
+class SobreWidget extends StatelessWidget {
+  const SobreWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        elevation: 4,
+        margin: EdgeInsets.all(16),
