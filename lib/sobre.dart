a3b73a728ecfadf88f314e2af1084090a740d45d
Crystyanno <crystyanno.alves@gmail.com>
2025-08-31 16:34:07 -0300

    atualização do sobre

---
+++ lib/sobre.dart
@@ -0,0 +1,10 @@
+//Por que escolher a Elity Drywall?
+//Com anos de experiência no mercado, oferecemos soluções completas em gesso acartonado com foco na qualidade, praticidade e satisfação do cliente.
+
+//Profissionais qualificados e experientes
+//Materiais de primeira qualidade
+//Projetos personalizados
+//Prazos cumpridos rigorosamente
+//Garantia em todos os serviços
+//Orçamento sem compromisso
+//Fale Conosco - neste campo será um button
